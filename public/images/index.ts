4a7d08e72b1ee88d7cdc057325508c2e414d4500
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-04-07 20:48:20 +0100

    feat: Add new SVG assets for Bug icon and update index exports

---
+++ public/images/index.ts
@@ -0,0 +1,6 @@
+import transforming from "./transforming.png";
+import whyEgnis from "./why-egnis.png";
+import eHealth from "./e-health.png";
+import nigeriaFlag from "./nigeria-flag.png";
+
+export { transforming, whyEgnis, eHealth, nigeriaFlag };
--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,3 +1,33 @@
 import egnisLogoIcon from "./logo.svg";
+import egnisLogoIcon2 from "./logo-egnis.svg";
+import amazon from "./amazon.svg";
+import arrowRightWhite from "./ArrowRight.svg";
+import arrowRightGreen from "./ArrowRightGreen.svg";
+import bug from "./Bug.svg";
+import dribble from "./dribble.svg";
+import egnisLogoBig from "./egnis-big-logo.svg";
+import google from "./google.svg";
+import lenovo from "./lenovo.svg";
+import microsoft from "./microsoft.svg";
+import netflix from "./netflix.svg";
+import security from "./security.svg";
+import slack from "./slack.svg";
+import youtube from "./youtube.svg";
 
-export { egnisLogoIcon };
+export {
+  egnisLogoIcon,
+  egnisLogoIcon2,
+  amazon,
+  arrowRightGreen,
+  arrowRightWhite,
+  bug,
+  dribble,
+  egnisLogoBig,
+  google,
+  lenovo,
+  microsoft,
+  netflix,
+  security,
+  slack,
+  youtube,
+};
