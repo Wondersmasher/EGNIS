5b6ddf56dff06d63e4646dab7a3f532e9b4701a4
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-04-08 11:08:35 +0100

    feat: Add background images to the export list in index.ts

--- public/images/index.ts
+++ public/images/index.ts
@@ -2,5 +2,7 @@ import transforming from "./transforming.png";
 import whyEgnis from "./why-egnis.png";
 import eHealth from "./e-health.png";
 import nigeriaFlag from "./nigeria-flag.png";
+import background from "./Background.png";
+import bgImgAlone from "./bg-img-alone.png";
 
-export { transforming, whyEgnis, eHealth, nigeriaFlag };
+export { transforming, whyEgnis, eHealth, nigeriaFlag, background, bgImgAlone };
