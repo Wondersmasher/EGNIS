87e4c95dcafb8f12ee2828f4c4b9976578b0d7a7
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-04-08 19:25:28 +0100

    feat: Re-export icons from Icons module in index.ts

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -14,6 +14,7 @@ import security from "./security.svg";
 import slack from "./slack.svg";
 import youtube from "./youtube.svg";
 
+export * from "./Icons";
 export {
   egnisLogoIcon,
   egnisLogoIcon2,
